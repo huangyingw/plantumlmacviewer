354298f542a9e4b211b1889b8d8b71478c213f4e
Ying Huang <huangyingw@gmail.com>
2024-01-31 19:02:25 -0800

    n

--- logger.py
+++ logger.py
@@ -1,9 +1,8 @@
 import logging
 import os
 
-
 def setup_logging():
-    log_file = os.path.expanduser("~/plantumlmacviewer.log")
+    log_file = os.path.expanduser("~/plantumlviewerserver.log")
     logging.basicConfig(
         filename=log_file,
         level=logging.INFO,
