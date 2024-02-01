8ce18d3a98927a1c3be38d443ab3f4ee3ff448ed
Ying Huang <huangyingw@gmail.com>
2024-01-31 18:23:06 -0800

    n

--- events.py
+++ events.py
@@ -4,6 +4,6 @@ from PyQt5.QtCore import QEvent
 class OpenWindowEvent(QEvent):
     EVENT_TYPE = QEvent.Type(QEvent.registerEventType())
 
-    def __init__(self, filePath):
+    def __init__(self, filePaths):
         super().__init__(OpenWindowEvent.EVENT_TYPE)
-        self.filePath = filePath
+        self.filePaths = filePaths  # filePaths 现在是一个列表
