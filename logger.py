0aed24fa1149c4f014e89804ea60c5c8ddadb425
Ying Huang <huangyingw@gmail.com>
2024-02-04 22:16:25 -0800

    n

--- logger.py
+++ logger.py
@@ -3,7 +3,13 @@ import os
 
 
 def setup_logging():
-    log_file = os.path.expanduser("~/plantumlviewerserver.log")
+    # 获取当前文件的绝对路径
+    current_file_path = os.path.abspath(__file__)
+    # 获取当前文件所在的目录
+    current_dir = os.path.dirname(current_file_path)
+    # 创建日志文件路径，位于当前目录下
+    log_file = os.path.join(current_dir, "plantumlviewerserver.log")
+
     logging.basicConfig(
         filename=log_file,
         level=logging.INFO,
